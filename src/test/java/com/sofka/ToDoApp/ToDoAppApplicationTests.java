7ccff265a9f33e6acc6f3f9d61e2ae7d3ece92e8
Angel Agelvis <agelvisangel@gmail.com>
2022-05-05 17:31:26 -0500

    ADD - Note Entity

---
+++ src/test/java/com/sofka/ToDoApp/ToDoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofka.ToDoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
